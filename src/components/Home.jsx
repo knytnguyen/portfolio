1e7adbede9963988926e18418969f8c9ca21c3fa
Kenny Nguyen <knytnguyen@gmail.com>
2022-10-28 16:39:17 -0700

    Created social icons and home page components

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#283550]'>
+        {/* CONTAINER */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Kaiyo,</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>an asipiring Software Engineer.</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>Welcome to my first ever React project! I hope you enjoy the stay ^_^</p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#e69fe9] hover:border-[#e8c9e9]'>
+                    View Work 
+                    <span className='group-hover:rotate-90 duration-300'>
+                        <HiArrowNarrowRight className='ml-3 '/>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -33,8 +35,29 @@ const Navbar = () => {
         </ul>
 
         {/* SOCIAL ICONS */}
-        <div className='hidden'>
-
+        <div className='hidden lg:flex fixed flex-column top-[35%] left-0'>
+            <ul>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+                    <a className='flex justify-between items-center w-full text-gray-300' href='/'>
+                        LinkedIn <FaLinkedin size={30} />
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' href='/'>
+                        GitHub <FaGithub size={30} />
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#5E81AC]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' href='/'>
+                        Email <HiOutlineMail size={30} />
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#B48EAD]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' href='/'>
+                        Resume <BsFillPersonLinesFill size={30} />
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
   )
