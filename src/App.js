e6b194f03311f151ddbbe89dfa4090be73e55220
Kenny Nguyen <knytnguyen@gmail.com>
2022-10-29 17:18:46 -0700

    Created portfolio section and uploaded portfolio images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Home from "./components/Home"
 import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 import Skills from "./components/Skills";
 
 
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Portfolio />
     </div>
   );
 }
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import TypingRoyale from '../assets/TypingRoyale-Image.PNG'
+// import FCC from '../assets/FCC-Logo.png'
+
+const Portfolio = () => {
+  return (
+    <div className='w-full md:h-screen text-gray-300 bg-[#2a3566]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Portfolio</p>
+                <p className='py-6'>Check out some of the projects I've done in the past!</p>
+            </div>
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+                <div style={{backgroundImage: `url(${TypingRoyale})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* HOVER EFFECTS */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
