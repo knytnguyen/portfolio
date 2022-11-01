14c8cac1cc23a14001ec9109213d34f2304a562c
Kenny Nguyen <knytnguyen@gmail.com>
2022-11-01 00:58:14 -0700

    Added more projects and images into portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import TypingRoyale from '../assets/TypingRoyale-Image.PNG'
-// import FCC from '../assets/FCC-Logo.png'
+import RogueOrigins from '../assets/RogueOrigins-Image.png'
+import BeginningPortfolio from '../assets/Beginning-Portfolio-Site.PNG'
+import CodePenProfile from '../assets/CodePen-Profile.PNG'
+import FCCProfile from '../assets/FreeCodeCamp-Profile.PNG'
 
 const Portfolio = () => {
   return (
@@ -10,9 +13,9 @@ const Portfolio = () => {
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Portfolio</p>
                 <p className='py-6'>Check out some of the projects I've done in the past!</p>
             </div>
-
+            {/* CONTAINER */}
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-
+                {/* GRID ITEM 1 */}
                 <div style={{backgroundImage: `url(${TypingRoyale})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* HOVER EFFECTS */}
@@ -30,6 +33,82 @@ const Portfolio = () => {
                         </div>
                     </div>
                 </div>
+                {/* GRID ITEM 2 */}
+                <div style={{backgroundImage: `url(${RogueOrigins})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* HOVER EFFECTS */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                {/* GRID ITEM 3 */}
+                <div style={{backgroundImage: `url(${BeginningPortfolio})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* HOVER EFFECTS */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* GRID ITEM 4 */}
+                <div style={{backgroundImage: `url(${CodePenProfile})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* HOVER EFFECTS */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* GRID ITEM 5 */}
+                <div style={{backgroundImage: `url(${FCCProfile})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* HOVER EFFECTS */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+                
             </div>
         </div>
     </div>
