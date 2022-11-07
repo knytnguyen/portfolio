61a17ee42468b4e2fba245f29787489eb1c2aec6
Kenny Nguyen <knytnguyen@gmail.com>
2022-11-06 17:14:47 -0800

    Created button to view resume in skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,7 +16,7 @@ const Skills = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-[#ff79c6]'>Skills & Resume</p>
                 <p className='py-4'>These are the technologies that I've worked with.</p>
             </div>
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8 pb-10'>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={CPP} alt="C++ Icon" className='w-20 mx-auto'/>
                     <p className='my-4'>C++</p>
@@ -42,7 +42,9 @@ const Skills = () => {
                     <p className='my-4'>HTML/CSS</p>
                 </div>
             </div>
-            <a href=''><button className='text-4xl border-2 border-[#f0f4f8] hover:bg-[#ff79c6] hover:text-[#111a29] p-2'>View Resume</button></a>
+            <a href='../assets/resume/Resume%20-%20Kenny%20Nguyen.pdf' rel='noreferrer' target='_blank'>
+                <button className='text-4xl border-2 border-[#f0f4f8] hover:bg-[#ff79c6] hover:text-[#111a29] p-2 flex justify-center mx-auto'>View Resume</button>
+            </a>
         </div>
     </div>
   )
