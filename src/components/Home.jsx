bf94b1deb6d90cd7533c63cc11de9b762ee1286c
Kenny Nguyen <knytnguyen@gmail.com>
2022-11-19 17:34:35 -0800

    Edited typing animations effects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,44 +4,19 @@ import { useState } from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import {Link} from 'react-scroll'
 
-const subtext = ['This is the subtext', 'Next subtext']
+import { useTypedSubtext } from './useTypedSubtext'
 
-const useTypedSubtext = (subtext: string[]) => {
-    const [typedSubtext, setTypedSubtext] = useState('')
-    useEffect(() => {
-     const nextTypedSubtext = subtext.slice(0, typedSubtext.length + 1)
-  
-     if (nextTypedSubtext === typedSubtext) return
-  
-     const timeout = setTimeout(() => {
-          setTypedSubtext(subtext.slice(0, typedSubtext.length + 1))
-      }, 150)
-      return () => clearTimeout(timeout)
-    }, [typedSubtext, typedSubtext])
-
-    return typedSubtext
-}
+const subtexts = ['This is the subtext', 'Next subtext']
 
 const Home = () => {
-  const subtext = useTypedSubtext(subtext)
-  const [typedSubtext, setTypedSubtext] = useState('')
-  useEffect(() => {
-   const nextTypedSubtext = subtext.slice(0, typedSubtext.length + 1)
-
-   if (nextTypedSubtext === typedSubtext) return
-
-   const timeout = setTimeout(() => {
-        setTypedSubtext(subtext.slice(0, typedSubtext.length + 1))
-    }, 150)
-    return () => clearTimeout(timeout)
-  }, [typedSubtext])
+  const subtext = useTypedSubtext(subtexts)
   return (
     <div name='home' className='w-full h-screen bg-gradient-to-r from-[#2A5470] to-[#4C4177]'>
         {/* CONTAINER */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-3xl py-2 text-[#88C0D0]'>Hi! My name is</p>
             <h2 className='text-4xl font-bold py-2 sm:text-7xl text-[#f0f4f8]'> Kenny Nguyen,</h2>
-            <h2 className='text-4xl py-2 sm:text-7xl font-bold text-[#8892b0] blinking-cursor'>{typedSubtext}</h2>
+            <h2 className='text-4xl py-2 sm:text-7xl font-bold text-[#8892b0] blinking-cursor'>{subtext}</h2>
             <p className='text-2xl text-[#8892b0] py-2 max-w-[700px]'>Welcome to my first ever React project! I hope you enjoy the stay ^_^</p>
             <div>
                 <button className='text-[#f0f4f8] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#9285C8] hover:border-[#121212]'>
