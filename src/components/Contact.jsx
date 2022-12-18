be2e547a244491b263553bab00f8b2912e6bffd0
Kenny Nguyen <knytnguyen@gmail.com>
2022-12-18 04:45:58 -0800

    Edited styling and colors of contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#152037] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-screen bg-[#FDFAF2] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/d4246a12-448e-4119-b6fa-4382a2c0ffbd" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-[#9285C8] text-[#f0f4f8]'>Contact</p>
-                <p className='text-[#f0f4f8] py-4'>Submit this form here or feel free to email me at: knytnguyen@gmail.com</p>
+                <p className='text-4xl font-bold inline border-b-4 border-[#9285C8] text-[#16192C]'>Contact</p>
+                <p className='text-[#16192C] py-4'>Submit this form here or feel free to email me at: knytnguyen@gmail.com</p>
             </div>
-            <input className='p-3 bg-[#ccdf6] rounded-lg' type='text' placeholder='Name' name='name' required/>
-            <input className='my-4 p-3 bg-[#ccdf6] rounded-lg' type='email' placeholder='Email' name='email'/>
-            <textarea className='p-3 bg-[#ccdf6] rounded-lg' name='message' rows='10' placeholder='Input your message here.'></textarea>
-            <button className='text-[#f0f4f8] border-2 hover:bg-[#9285C8] hover:border-[#152037] px-4 py-3 my-8 mx-auto flex items-center' required>Send Message</button>
+            <input className='p-3 bg-[#FFF5B1] rounded-lg border-2 border-solid border-[#16192C]' type='text' placeholder='Name' name='name' required/>
+            <input className='my-4 p-3 bg-[#FFF5B1] rounded-lg border-2 border-solid border-[#16192C]' type='email' placeholder='Email' name='email'/>
+            <textarea className='p-3 bg-[#FFF5B1] rounded-lg border-2 border-solid border-[#16192C]' name='message' rows='10' placeholder='Input your message here.'></textarea>
+            <button className='text-[#16192C] bg-[#A2D6E7] hover:bg-[#E6CCF5] border-2 border-[#121212] px-4 py-3 my-8 mx-auto flex items-center' required>Send Message</button>
         </form>
     </div>
   )
