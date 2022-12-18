8b9ddd1e8f76592d656790baead6a1535ad0a174
Kenny Nguyen <knytnguyen@gmail.com>
2022-12-18 04:54:31 -0800

    Edited styling of containers and colors of skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,7 @@ import HTML from '../assets/skills/HTML-Logo.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#152037] text-[#f0f4f8]'>
+    <div name='skills' className='w-full h-screen bg-[#FDFAF2] text-[#16192C]'>
         {/* CONTAINER */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
@@ -17,33 +17,33 @@ const Skills = () => {
                 <p className='py-4'>These are the technologies that I've worked with.</p>
             </div>
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8 pb-10'>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={CPP} alt="C++ Icon" className='w-20 mx-auto'/>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#FFF5B1]'>
+                    <img src={CPP} alt="C++ Icon" className='w-20 mx-auto pt-2'/>
                     <p className='my-4'>C++</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={Python} alt="C++ Icon" className='w-20 mx-auto'/>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#FFF5B1]'>
+                    <img src={Python} alt="C++ Icon" className='w-20 mx-auto pt-2'/>
                     <p className='my-4'>Python</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={JavaScript} alt="C++ Icon" className='w-20 mx-auto'/>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#FFF5B1]'>
+                    <img src={JavaScript} alt="C++ Icon" className='w-20 mx-auto pt-1'/>
                     <p className='my-4'>JavaScript</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#FFF5B1]'>
                     <img src={GitHub} alt="C++ Icon" className='w-20 mx-auto'/>
                     <p className='my-4'>Git/GitHub</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#FFF5B1]'>
                     <img src={ReactLogo} alt="C++ Icon" className='w-20 mx-auto'/>
                     <p className='my-4'>React</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img src={HTML} alt="C++ Icon" className='w-20 mx-auto'/>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#FFF5B1]'>
+                    <img src={HTML} alt="C++ Icon" className='w-20 mx-auto pt-2'/>
                     <p className='my-4'>HTML/CSS</p>
                 </div>
             </div>
             <a href='../assets/resume/Resume%20-%20Kenny%20Nguyen.pdf' rel='noreferrer' target='_blank'>
-                <button className='text-lg border-2 border-[#f0f4f8] hover:bg-[#9285C8] hover:border-[#152037] p-4 flex justify-center mx-auto'>View Resume</button>
+                <button className='text-[#16192C] bg-[#A2D6E7] group border-2 px-6 py-3 my-2 flex justify-center mx-auto border-[#121212] hover:bg-[#E6CCF5]'>View Resume</button>
             </a>
         </div>
     </div>
