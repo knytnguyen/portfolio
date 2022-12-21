9008ec503e575ec2a549c43f93fca466cad407d1
Kenny Nguyen <knytnguyen@gmail.com>
2022-12-20 20:52:04 -0800

    Made navbar more responsive to screen sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
                 <Link to="about" smooth={true} duration={500}> About </Link>
             </li>
             <li className='bg-gradient-to-r from-red-500 via-blue-400 to-lime-600 bg-[length:0%_5px] bg-no-repeat bg-left-bottom hover:bg-[length:100%_5px] transition-all duration-500 p-3'>
-                <Link to="skills" smooth={true} duration={500}> Skills & Resume </Link>
+                <Link to="skills" smooth={true} duration={500}> Skills/Resume </Link>
             </li>
             <li className='bg-gradient-to-r from-red-500 via-blue-400 to-lime-600 bg-[length:0%_5px] bg-no-repeat bg-left-bottom hover:bg-[length:100%_5px] transition-all duration-500 p-3'>
                 <Link to="portfolio" smooth={true} duration={500}> Portfolio </Link>
