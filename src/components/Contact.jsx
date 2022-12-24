f943c0a56fe52222a5d78100645ebb9295e00314
Kenny Nguyen <knytnguyen@gmail.com>
2022-12-24 02:57:52 -0800

    Changed color of email and removed round edges of input boxes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,12 +7,12 @@ const Contact = () => {
             <div className='pb-8'>
                 <h3 className='text-4xl font-bold inline bg-gradient-to-r from-red-500 via-blue-400 to-lime-600 bg-[length:100%_5px] bg-no-repeat bg-left-bottom p-1'>Contact</h3>
                 <p className='text-[#16192C] py-4'>For feedback or business inquiries, submit this form here or send me an email at: ‎  
-                  <a className='bold underline text-[#B48EAD] hover:text-[#E6CCF5]' href="mailto: knytnguyen@gmail.com">knytnguyen@gmail.com</a>
+                  <a className='bold underline text-[#2EC58C] hover:text-[#E1306C]' href="mailto: knytnguyen@gmail.com">knytnguyen@gmail.com</a>
                 </p>
             </div>
-            <input className='p-3 bg-[#FFF5B1] rounded-lg border-2 border-solid border-[#16192C]' type='text' placeholder='Name' name='name' required/>
-            <input className='my-4 p-3 bg-[#FFF5B1] rounded-lg border-2 border-solid border-[#16192C]' type='email' placeholder='Email' name='email'/>
-            <textarea className='resize-none p-3 bg-[#FFF5B1] rounded-lg border-2 border-solid border-[#16192C]' name='message' rows='10' placeholder='Input your message here.'></textarea>
+            <input className='p-3 bg-[#FFF5B1] border-2 border-solid border-[#16192C]' type='text' placeholder='Name' name='name' required/>
+            <input className='my-4 p-3 bg-[#FFF5B1] border-2 border-solid border-[#16192C]' type='email' placeholder='Email' name='email'/>
+            <textarea className='resize-none p-3 bg-[#FFF5B1] border-2 border-solid border-[#16192C]' name='message' rows='10' placeholder='Input your message here.'></textarea>
             <button className='text-[#16192C] bg-[#A2D6E7] hover:bg-[#E6CCF5] border-2 border-[#121212] px-4 py-3 my-8 mx-auto flex items-center' required>Send Message</button>
         </form>
     </div>
