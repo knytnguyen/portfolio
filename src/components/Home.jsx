cae730c5baefe9bc88391f8f5728be1f84eeff47
Kenny Nguyen <knytnguyen@gmail.com>
2022-12-25 03:39:30 -0800

    Removed react-typical package due to lag issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import {Link} from 'react-scroll'
-import Typical from 'react-typical'
 
 const Home = () => {
   return (
@@ -12,7 +11,7 @@ const Home = () => {
 
             <h2 className='text-4xl font-bold py-3 sm:text-7xl text-[#16192C]'> Kenny Nguyen,</h2>
 
-            <p className='text-4xl py-3 sm:text-5xl font-bold text-[#16192C]'>
+            {/* <p className='text-4xl py-3 sm:text-5xl font-bold text-[#16192C]'>
                 I am a
                 <Typical
                     wrapper="b"
@@ -23,7 +22,7 @@ const Home = () => {
                         ' software developer.', 3000
                     ]}
                 />
-            </p>
+            </p> */}
 
             <p className='text-2xl text-[#16192C] py-2 max-w-[700px]'>Welcome to my first ever <span className='font-bold inline bg-gradient-to-r from-red-500 via-blue-400 to-lime-600 bg-[length:100%_5px] bg-no-repeat bg-left-bottom'>React.js and Tailwind CSS</span> project! I hope you enjoy the stay ^_^</p>
 
