6d9631ce25d490b5178fd24a8ac8f0f1fdf7bdbf
Kenny Nguyen <knytnguyen@gmail.com>
2022-12-26 14:12:00 -0800

    Adjusted padding of contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#FDFAF2] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/d4246a12-448e-4119-b6fa-4382a2c0ffbd" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
+            <div className='pb-3'>
                 <h3 className='text-4xl font-bold inline bg-gradient-to-r from-red-500 via-blue-400 to-lime-600 bg-[length:100%_5px] bg-no-repeat bg-left-bottom p-1'>Contact</h3>
                 <p className='text-[#16192C] py-4'>For feedback or business inquiries, submit this form here or send me an email at: ‎  
                   <a className='bold underline text-[#E1306C] hover:text-[#2EC58C]' href="mailto: knytnguyen@gmail.com">knytnguyen@gmail.com</a>
