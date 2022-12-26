dc6c9ff3941c3ce3115762e7f5226294d03baa27
Kenny Nguyen <knytnguyen@gmail.com>
2022-12-26 13:57:23 -0800

    Added animate on scroll to skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css'; 
 
 import CPP from '../assets/skills/CPP-Logo.png';
 import Python from '../assets/skills/Python-Logo.png';
@@ -7,16 +9,21 @@ import GitHub from '../assets/skills/GitHub-Logo.png';
 import ReactLogo from '../assets/skills/React-Logo.png';
 import HTML from '../assets/skills/HTML-Logo.png';
 
+
+
 const Skills = () => {
+  useEffect(() => {
+    Aos.init({ duration : 2000 });
+    }, []);
   return (
     <div name='skills' className='w-full h-screen bg-[#FDFAF2] text-[#16192C]'>
         {/* CONTAINER */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div>
+            <div data-aos="fade-down" data-aos-once="true">
                 <h3 className='text-4xl font-bold inline bg-gradient-to-r from-red-500 via-blue-400 to-lime-600 bg-[length:100%_5px] bg-no-repeat bg-left-bottom p-1'>Skills & Resume</h3>
                 <p className='py-4'>These are some the technologies that I've worked with.</p>
             </div>
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8 pb-10'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8 pb-10' data-aos="fade-down" data-aos-once="true">
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#FFF5B1]'>
                     <img src={CPP} alt="C++ Icon" className='w-20 mx-auto pt-2'/>
                     <p className='my-4'>C++</p>
