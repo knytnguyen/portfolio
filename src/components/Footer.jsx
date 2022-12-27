deae2885329a227a965b618775d7f5c0d7450b11
Kenny Nguyen <knytnguyen@gmail.com>
2022-12-26 20:44:24 -0800

    Changed colors of the footer for legibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import {BsSuitHeartFill} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div className='w-full h-max bg-[#E6CCF5] block absolute text-center p-4'>
+    <div className='w-full h-max bg-[#121212] text-[#f0f4f8] block absolute text-center p-4'>
         <div className='align-middle py-2'>
-            <span className='text-[#16192C]'> Made with <BsSuitHeartFill className='inline' color='red' size={15} />‎  
+            <span> Made with <BsSuitHeartFill className='inline' color='red' size={15} />‎  
             in <a href='https://reactjs.org/' target='_blank' rel="noreferrer" className='underline font-extrabold cursor-pointer'>React.js</a>‎  
             and <a href='https://tailwindcss.com/' target='_blank' rel="noreferrer" className='underline font-extrabold cursor-pointer'>Tailwind CSS</a>.
             Deployed via <a href='https://pages.github.com/' target='_blank' rel="noreferrer" className='underline font-extrabold cursor-pointer'>GitHub Pages</a>.
