c77bb5579589fc4197e7af9df71f75adfdd17c8f
Kenny Nguyen <knytnguyen@gmail.com>
2024-09-03 13:48:45 +0700

    Created experiences data file to hold past work experience

---
+++ src/data/experiences.js
@@ -0,0 +1,38 @@
+const experiences = [
+    {
+      company: 'Keywords Studios',
+      role: 'AI Research Intern',
+      date: 'Jun 2023 - Jan 2024',
+      location: 'San Francisco, CA',
+      description: [
+        'Led the development and quality assurance of 10,000+ data points for machine learning training datasets using software tools such as Appen and Scale AI.',
+        'Collaborated with engineers and researchers to pinpoint and resolve technical issues, enhancing multimodal performance by ~70%.',
+        'Developed Python test scripts and documented technical optimization strategies in Notion to ensure system reliability.'
+      ],
+    },
+    {
+      company: 'UC Riverside - Dept. of Statistics',
+      role: 'Data Assistant',
+      date: 'Mar 2022 - Sep 2022',
+      location: 'Riverside, CA',
+      description: [
+        'Compiled, categorized, and reviewed 1000+ data instances pertaining to student experience of teaching surveys.',
+        'Assisted with integrating data points into a machine learning model that helps to evaluate the quality of education in the University of California system.',
+        'Collaborated with team members on data categorization and optimized database organization for improved access.'
+      ],
+    },
+    {
+      company: 'Cutie Hack 2020',
+      role: 'Development Team Member',
+      date: 'Nov 2020',
+      location: 'Sacramento, CA',
+      description: [
+        'Placed 2nd overall out of 500+ participants on a team of 4 during UCR’s 12 hour hackathon competition.',
+        'Created a typing application in the Godot Game Engine that puts the user up against a self-typing bot opponent in a match determined by the player that achieves the greater WPM (words per minute).',
+        'Developed the backend of the application with the use of GDScript, a high-level, dynamically typed programming language with similar syntax to Python.'
+      ],
+    }
+  ];
+  
+  export default experiences;
+  
